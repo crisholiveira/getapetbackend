7582432cce70d2c81bc9429fcb97ce565dacbe8e
crisholiveira <cris.ho.soares@gmail.com>
2022-05-19 10:38:59 -0300

    iniciando validacoes de atualizacao de usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require('../models/Users')
 const bcrypt = require('bcrypt')
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 const jwt = require('jsonwebtoken')
 
 
@@ -115,6 +116,50 @@ module.exports = class UserController {
     }
 
     static async editUser(req, res) {
-        res.status(200).json({ message: "Deu certo o update"})
+       
+
+        const token = getToken(req)
+
+        const user = await getUserByToken(token)
+
+        const {name, email, phone, password, confirmpassword} = req.body 
+
+        let image = ' '
+
+        if(!name) {
+            res.status(422).json({message: 'O nome é obrigatório'})
+            return
+        }
+        user.name = name
+
+        if(!email) {
+            res.status(422).json({message: 'O e-mail é obrigatório'})
+            return
+        }
+        const userExists = await User.findOne({email: email})
+
+        if(user.email !== email && userExists) {
+            res.status(422).json({ message: "Por favor, utilize outro e-mail"})
+            return
+        }
+
+        user.email = email
+
+        if(!phone) {
+            res.status(422).json({message: 'O telefone é obrigatório'})
+            return        
+        }
+        user.phone = phone
+        if(!password) {
+            res.status(422).json({message: 'A senha é obrigatória'})
+            return
+        }
+        if(!confirmpassword) {
+            res.status(422).json({message: 'A confrimação de senha é obrigatória'})
+            return
+        }
+        if(password !== confirmpassword) {
+            res.status(422).json({message: 'A senha e a confirmação de senha devem ser iguais'})
+        }         
     }
 }
